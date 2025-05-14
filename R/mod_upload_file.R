f28153bdd40d90ed7b660e6ef9456c7944a8f355
akhileshsivaraman <akhileshsiva@aol.com>
2025-05-14 16:20:11 +0100

    initial commit

---
+++ R/mod_upload_file.R
@@ -0,0 +1,21 @@
+#----- mod_file_upload.R -----
+library(shiny)
+library(bslib)
+
+#---- file_upload_UI ----
+file_upload_UI <- function(id){
+  tagList(
+    
+  )
+}
+
+#---- file_upload_server ----
+file_upload_server <- function(id){
+  moduleServer(id, function(input, output, session){
+    
+    
+  })
+}
+
+
+#---- file_upload_app ----
