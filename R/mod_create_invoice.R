4af6f517a76290f6aecc62cbcc556162e543f75f
akhileshsivaraman <akhileshsiva@aol.com>
2025-08-30 20:31:39 +0100

    creating create_invoice module

---
+++ R/mod_create_invoice.R
@@ -0,0 +1,141 @@
+#----- mod_create_invoice.R -----
+
+#---- create_invoice_UI ----
+create_invoice_UI <- function(id){
+  tagList(
+    card(
+      fill = FALSE,
+      
+      fluidRow(
+        column(
+          12,
+          h3("Create Invoice"),
+          
+          selectInput(
+            NS(id, "select_term"),
+            label = "Select Term",
+            choices = c(
+              "Autumn",
+              "Spring",
+              "Summer"
+            )
+          ),
+          
+          numericInput(
+            NS(id, "individual_coaching_court_fee"),
+            label = "1-to-1 coaching hours"
+          ),
+          
+          downloadButton(
+            NS(id, "create_invoice"),
+            label = "Create Invoice"
+          )
+        )
+      )
+    )
+  )
+}
+
+#---- create_invoice_server ----
+create_invoice_server <- function(id){
+  moduleServer(id, function(input, output, session){
+    
+    output$create_invoice <- downloadHandler(
+      filename = function(){
+        year <- year(Sys.Date())
+        paste0(input$term, "_", year, ".xlsx")
+      },
+      
+      content = function(file){ # may require the group_coaching_table created in mod_court_fees_table but how to serve it up? use the r object?
+        
+        #--- calculate subtotals
+        group_coaching_court_fee <- sum(group_coaching_table()[["Court fee subtotal"]])
+        non_member_court_fee <- sum(group_coaching_table()[["Non-member fee subtotal"]])
+        fees <- read_json("data/fees.json", simplifyVector = TRUE)
+        individual_coaching_court_fee <- input$individual_coaching_court_fee * fees$individial_coaching_hourly_court_fee
+        
+        
+        #--- construct workbook ---
+        wb <- wb_workbook()
+        
+        # make four smaller tables then stitch them together into the worksheet using dims arg
+        heading_table <- data.frame(
+          "Raynes Park Residents LTC" = c(
+            "Invoice from the head coach for the court usage and non-member supplement",
+            "This invoice is sent at the end of each coaching term and requires payment by the head coach within 21 days of the invoice date."
+          ),
+          check.names = FALSE
+        )
+        
+        inputs_table <- data.frame(
+          "Term" = input$select_term,
+          "Court fee per hour of group coaching" = fees$group_coaching_hourly_court_fee,
+          "Court fee per hour of 1-2-1 coaching" = fees$individial_coaching_hourly_court_fee,
+          "Fee per non-member per lesson" = fees$non_member_fee_per_lesson,
+          check.names = FALSE
+        )
+        
+        sub_totals_table <- data.frame(
+          "Group coaching court fee" = group_coaching_court_fee,
+          "1-2-1 coaching court fee" = individual_coaching_court_fee,
+          "Non-member court fee" = non_member_court_fee,
+          check.names = FALSE
+        )
+        
+        total_table <- data.frame(
+          "Total" = group_coaching_court_fee + individual_coaching_court_fee + non_member_court_fee,
+          check.names = FALSE
+        )
+        
+        # fill in workbook
+        wb$
+          add_worksheet(sheet = "Total Fees")$
+          add_data(
+            sheet = "Total Fees",
+            x = heading_table,
+            dims = "A1"
+          )$
+          add_data(
+            sheet = "Total Fees",
+            x = inputs_table,
+            dims = "A5"
+          )$
+          add_data( # TODO: add subtotal header
+            sheet = "Total Fees",
+            x = sub_totals_table,
+            dims = "A9"
+          )$
+          add_data( # TODO: add total header / make bold
+            sheet = "Total Fees",
+            x = total_table,
+            dims = "A13"
+          )$
+          add_worksheet(sheet = "Group Coaching")$
+          add_data(
+            sheet = "Group Coaching",
+            x = group_coaching_table
+          )
+        
+        wb_save(wb, file)
+      }
+    )
+  })
+}
+
+
+#---- create_invoice_app ----
+create_invoice_app <- function(){
+  ui <- page_fluid(
+    create_invoice_UI("create_invoice")
+  )
+  
+  server <- function(input, output, session){
+    source("workings/OOP testing.R")
+    source("workings/create xlsx.R")
+    create_invoice_server("create_invoice") # need inputs from the UI too
+  }
+  
+  shinyApp(ui, server)
+}
+
+create_invoice_app()
